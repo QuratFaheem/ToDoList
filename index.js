0191d78a3c00d51acc0d44cde2e2e2647bace7a1
QuratFaheem <mrs.faheem16@gmail.com>
2024-04-06 02:25:38 +0500

    its first trial

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let mytodolist = [];
+let asktoAdd = true;
+while (asktoAdd) {
+    const newtask = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What would you like to add in your to do list?",
+        },
+        {
+            type: 'confirm',
+            name: 'addconfirm',
+            message: "Do you want to Add more?",
+            default: "false",
+        },
+    ]);
+    mytodolist.push(newtask.name);
+    asktoAdd = newtask.addconfirm;
+    console.log(mytodolist);
+}
